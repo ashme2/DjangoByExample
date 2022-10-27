a0cbc436e003d40cc9f125094f89b7de422d14ce
ashraf <ashcairoeng@gmail.com>
2022-10-27 13:01:43 +0200

    Ch02-P02: Creating a comment system

--- src/myblog/models.py
+++ src/myblog/models.py
@@ -1,7 +1,9 @@
-from django.db import models
-from django.utils import timezone
+from email.policy import default
 from django.contrib.auth.models import User
+from django.db import models
 from django.urls import reverse
+from django.utils import timezone
+
 
 # Create your models here.
 class PublishedManager (models.Manager):
@@ -30,3 +32,18 @@ class Post (models.Model):
     published = PublishedManager()  # Our custom manager
     def get_absolute_url(self):
         return reverse('myblog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- src/myblog/views.py
+++ src/myblog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
-from django.core.paginator import Paginator, EmptyPage,\
-    PageNotAnInteger
-from django.views.generic import ListView
-from .forms import EmailPostForm
 from django.core.mail import send_mail
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
+
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
 
 # Create your views here.
 
@@ -33,7 +33,26 @@ class PostListView (ListView):
 
 def post_detail (request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'myblog/post/detail.html', {'post': post})
+    
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'myblog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
 
 def post_share (request, post_id):
     # Retrieve post by id
