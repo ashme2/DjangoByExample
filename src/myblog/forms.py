043162516422c337ac9be2303b4249666cd98044
ashraf <ashcairoeng@gmail.com>
2022-10-27 07:08:49 +0200

    Remove Error Data

--- src/myblog/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django import forms
-
-class EamilPostForm (forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
