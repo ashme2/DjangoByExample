434ea4ea2b87fc7248c13db44ea27ff1495b4f36
ashraf <ashcairoeng@gmail.com>
2022-10-29 15:27:51 +0200

    Ch02-P04: Retrieving posts by similarity

--- src/myblog/views.py
+++ src/myblog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from taggit.models import Tag
+from django.db.models import Count
 
 from .forms import EmailPostForm, CommentForm
 from .models import Post, Comment
@@ -61,7 +62,14 @@ def post_detail (request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
-    return render(request, 'myblog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'myblog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form, 'similar_posts': similar_posts})
 
 def post_share (request, post_id):
     # Retrieve post by id
